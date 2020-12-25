2bfab39da01c7d14f2dd3f4101991c4bb2c46f41
John Delizo <johndelizo99@gmail.com>
2020-12-25 08:19:20 -0800

    set up GET and POST requests

--- routes/api-Routes.js
+++ routes/api-Routes.js
@@ -5,6 +5,12 @@ const path = require("path");
 router.get("/", (req, res) => {
     res.json({ msg: "success"});
 });
+
+router.post("/", (req, res) => {
+    console.log(req.body);
+    res.status(201).send('Post Test')
+})
+    
   
 
 
