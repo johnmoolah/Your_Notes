21d950ad8ee0743d829c497d5b3ce00a069e984a
John Delizo <johndelizo99@gmail.com>
2020-12-25 14:20:21 -0800

    enabled db.json file to be written

--- routes/api-Routes.js
+++ routes/api-Routes.js
@@ -3,19 +3,28 @@ const fs = require("fs")
 const path = require("path");
 
 
+
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../clientRoutes/index.html"))
+    res.sendFile(path.join(__dirname, "../client/index.html"))
     console.log("Landing page")
 });
 
+
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../clientRoutes/notes.html"))
+    res.sendFile(path.join(__dirname, "../client/notes.html"))
     console.log("Got notes")
+
+fs.writeFile("db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log("File written")
+});
 });
 
-router.post("/", (req, res) => {
-    console.log(req.body);
-    res.status(201).send('Post Test')
+router.post("/notes", (req, res) => {
+    fs.readFile("db.json", (err, data) => {
+        if (err) throw err;
+        console.log("File read")
+            });
 });
     
 module.exports = router;
